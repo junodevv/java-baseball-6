b167e513b100937f3550924fb05b5387cd86fc96
leejuno <zhqkfxm0127@naver.com>
2023-10-28 23:29:27 +0900

    feat(baseball): 특정 자리에 숫자가 있는지 알 수 있는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,9 +18,13 @@ public class Application {
 //        NumberGenerator generator = new NumberGenerator();
 //        List<Integer> numbers = generator.createRandomNumbers();
 //        System.out.println(numbers);
+
         Judgment judgment = new Judgment();
-        int count = judgment.correctCount(Arrays.asList(1, 5, 3), Arrays.asList(1, 2, 3));
-        System.out.println(count);
+//        int count = judgment.correctCount(Arrays.asList(1, 5, 3), Arrays.asList(1, 2, 3));
+//        System.out.println(count);
+
+        boolean place = judgment.hasPlace(Arrays.asList(7, 8, 9), 1, 8);
+        System.out.println(place);
     }
 }
 
--- src/main/java/baseball/domain/Judgment.java
+++ src/main/java/baseball/domain/Judgment.java
@@ -3,17 +3,22 @@ package baseball.domain;
 import java.util.List;
 
 public class Judgment {
-    public int correctCount(List<Integer> computer, List<Integer> player){
+    public int correctCount(List<Integer> computer, List<Integer> player) {
         int result = 0;
-        for(int i = 0; i < player.size(); i++){
+        for (int i = 0; i < player.size(); i++) {
             int playerNumber = player.get(i);
-            if(computer.contains(playerNumber)){
+            if (computer.contains(playerNumber)) {
                 result++;
             }
         }
         return result;
     }
-    public boolean hasPlace(List<Integer> computer, int placeNumber, int number){
-        return false;
+
+    public boolean hasPlace(List<Integer> computer, int placeIndex, int number) {
+        return computer.get(placeIndex)==number;
+//        if (computer.get(placeIndex)==number) {
+//            return true;
+//        }
+//        return false;
     }
 }
