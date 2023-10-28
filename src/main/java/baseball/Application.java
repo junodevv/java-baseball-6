cab9f756a201811b3fd52a602febb946042aee11
leejuno <zhqkfxm0127@naver.com>
2023-10-28 23:44:31 +0900

    feat(baseball): 낫싱 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -28,7 +28,7 @@ public class Application {
 //        System.out.println(place);
 
         Referee referee = new Referee();
-        String result = referee.compare(Arrays.asList(3, 1, 2), Arrays.asList(1, 2, 3));
+        String result = referee.compare(Arrays.asList(3, 8, 9), Arrays.asList(1, 2, 3));
         System.out.println(result);
     }
 }
