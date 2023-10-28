5a9bf04dd5fef8aef1de932e4377f7ffa047d646
leejuno <zhqkfxm0127@naver.com>
2023-10-29 00:57:35 +0900

    refactor(baseball): 테스트 중복 제거

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class RefereeTest {
     private Referee referee;
@@ -16,6 +18,13 @@ class RefereeTest {
     void setUp() {
         referee = new Referee();
 
+    }
+
+    @ParameterizedTest // 여러가지 경우 테스트
+    @CsvSource({"1,2,3,0 볼 3 스트라이크", "7,8,9,낫싱", "2,3,1,3 볼 0 스트라이크"})// 이건 공부해보기
+    public void compare(int number1, int number2, int number3, String expected) {
+        String actual = referee.compare(ANSWER, Arrays.asList(number1, number2, number3));
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
